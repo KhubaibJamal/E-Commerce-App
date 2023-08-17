75f67b0bb1838eeebb59f2d3c257ced0fbc078ea
Khubaib Jamal <khubaib.j18@gmail.com>
2023-08-17 15:15:03 +0500

    setup firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:emart/views/splash_screen/splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import 'consts/consts.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
